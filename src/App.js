2660137a09b8ba92177d21e7e7cee67578993d95
parascoding <parascoding@gmail.com>
2021-05-09 22:25:44 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import Coin from './Coin';
+import React ,{useState, useEffect} from 'react'
+import axios from 'axios'
+function App() {
+const [coins,setCoins]=useState([])
+  useEffect(()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res=>{
+      setCoins(res.data);
+      console.log(res.data);
+    })
+    .catch(error=>console.log(error));
+  },[]);
+const[search, setSearch]=useState(''); 
+const handleChange=e=>{
+  setSearch(e.target.value);
+}
+const filteredCoins=coins.filter(coin =>
+  coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  return (
+    <div className="coin-app">
+      <div className='coin-search'>
+        <h1 className='coint-text'>
+          Search a currency
+        </h1>
+        <form >
+          <input type='text' placeholder='search' className='coint-input' onChange={handleChange}>
+          </input>
+        </form>
+      </div>
+      {filteredCoins.map(coin=>{
+        return(
+          <Coin 
+          key={coin.id} name={coin.name} image={coin.image} price={coin.current_price} symbol={coin.symbol}
+          />
+
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Coin.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Coin=({name, image, symbol, price, volume})=>{
+    return(
+        <div className='coin-container'>
+            <div className='coin-row'>
+                <div className='coin'>
+                <img src={image} alt="crypto"></img>
+                <h1>
+                    {name}
+                </h1>
+                <p className='coin-sybol'>{symbol}</p>
+                </div>
+                <div className='coin-data'>
+                    <p className='coin-price'>
+                        ${price}
+                    </p>
+                    {/* <p className='coin-volume'>
+                        ${volume.toLocaleString()}
+                    </p> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Coin
