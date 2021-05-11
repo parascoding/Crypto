9e20f07dd82d2a2516f73bc3c1ae679ace57dee9
parascoding <parascoding@gmail.com>
2021-05-11 21:44:27 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Coin from './Coin';
 import React ,{useState, useEffect} from 'react'
 import axios from 'axios'
 import './Coin.css'
-function App() {
+import Btc from './Btc'
+
+function App(props) {
 const [coins,setCoins]=useState([])
   useEffect(()=>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=1000&page=1&sparkline=false')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=5&page=1&sparkline=false')
     .then(res=>{
       setCoins(res.data);
       // console.log(res.data);
@@ -20,26 +22,32 @@ const handleChange=e=>{
 const filteredCoins=coins.filter(coin =>
   coin.name.toLowerCase().includes(search.toLowerCase())
   )
+const display=()=>{
+  <Btc />
+}
+return (
 
-  return (
-    <div className="coin-app">
-      <div className='coin-search'>
-        <h1 className='coint-text'>
-          Search a currency
-        </h1>
-        <form >
-          <input type='text' placeholder='Search' className='coin-input' onChange={handleChange}/>
-        </form>
-      </div>
-        {filteredCoins.map(coin=>{
-          return(
-            <Coin 
-            key={coin.id} name={coin.name} image={coin.image} price={coin.current_price} symbol={coin.symbol}
-            />
-          );
-        })}
+  <div className="coin-app">
+    <div className="button">
+      <button onClick={display}>Click me</button>
     </div>
-  );
+    <div className='coin-search'>
+      <h1 className='coint-text'>
+        Search a currency
+      </h1>
+      <form >
+        <input type='text' placeholder='Search' className='coin-input' onChange={handleChange}/>
+      </form>
+    </div>
+      {filteredCoins.map(coin=>{
+        return(
+          <Coin 
+          key={coin.id} name={coin.name} image={coin.image} price={coin.current_price} symbol={coin.symbol}
+          />
+        );
+      })}
+  </div>
+);
 }
 
 export default App;
---
+++ src/Btc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Btc(){
+    return(
+        <div>
+            My name 
+        </div>
+    );
+}
+export default Btc;
